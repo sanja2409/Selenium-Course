067cdc291905f691bff2530b681d333dcca02109
Anna Klekotko <sanja@op.pl>
2020-03-05 11:28:23 +0100

    Task 13 - update
    updated with loop and changed removing from the cart

--- src/test/java/selenium/training/TaskThirteen.java
+++ src/test/java/selenium/training/TaskThirteen.java
@@ -1,7 +1,6 @@
 package selenium.training;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,12 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
+
 import java.util.concurrent.TimeUnit;
-import static org.junit.Assert.assertTrue;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 public class TaskThirteen {
@@ -37,47 +34,36 @@ public class TaskThirteen {
         WebElement product = driver.findElement(By.xpath("//div[contains(@id,'box-latest-products')]//ul[contains(@class,'listing-wrapper')]//li[3]//div[starts-with(@class,'name')]"));
         wait.until(elementToBeClickable(product)).click();
         String addCartProduct = "add_cart_product";
+        int item = 0;
+        addToCart(addCartProduct, item);
 
-        // adding product to the cart
-        driver.findElement(By.name(addCartProduct)).click();
-
-        // checking if basket is updated
-        WebElement cartItem = driver.findElement(By.cssSelector("span.quantity"));
-        int itemsExpectedNumber = 1;
-        String itemsExpectedNumberAsString = Integer.toString(itemsExpectedNumber);
-        wait.until(attributeContains(cartItem,"textContent" , itemsExpectedNumberAsString));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        driver.findElement(By.className("general-0")).click();
         driver.findElement(By.xpath("//div[contains(@id,'box-latest-products')]//ul[contains(@class,'listing-wrapper')]//li[4]//div[starts-with(@class,'name')]")).click();
-        driver.findElement(By.name(addCartProduct)).click();
-        WebElement cartItem2 = driver.findElement(By.cssSelector("span.quantity"));
-        int itemsExpectedNumber2 = 2;
-        String itemsExpectedNumberAsString2 = Integer.toString(itemsExpectedNumber2);
-        wait.until(attributeContains(cartItem2,"textContent" , itemsExpectedNumberAsString2));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        int item1 = 1;
+        addToCart(addCartProduct, item1);
 
-        driver.findElement(By.className("general-0")).click();
         driver.findElement(By.xpath("//div[contains(@id,'box-latest-products')]//ul[contains(@class,'listing-wrapper')]//li[5]//div[starts-with(@class,'name')]")).click();
-        driver.findElement(By.name(addCartProduct)).click();
-        WebElement cartItem3 = driver.findElement(By.cssSelector("span.quantity"));
-        int itemsExpectedNumber3 = 3;
-        String itemsExpectedNumberAsString3 = Integer.toString(itemsExpectedNumber3);
-        wait.until(attributeContains(cartItem3,"textContent" , itemsExpectedNumberAsString3));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        int item2 = 2;
+        addToCart(addCartProduct, item2);
 
-        driver.findElement(By.className("general-0")).click();
         driver.findElement(By.cssSelector("div#cart")).click();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        // checking the value of the table
-        int itemAmount = 3 ;
+        wait.until(visibilityOfElementLocated(By.cssSelector("a.inact"))).click();
+        wait.until(visibilityOfElementLocated(By.name("remove_cart_item"))).click();
+        wait.until(visibilityOfElementLocated(By.cssSelector("a.act"))).click();
+        wait.until(visibilityOfElementLocated(By.name("remove_cart_item"))).click();
+        wait.until(visibilityOfElementLocated(By.name("remove_cart_item"))).click();
 
-        for (int i=0; i<itemAmount; i++)
-        {
-            wait.until(numberOfElementsToBe(By.cssSelector("td.item"), itemAmount--));
-            wait.until(visibilityOfElementLocated(By.name("remove_cart_item"))).click();
-        }
+    }
+
+    private void addToCart(String addCartProduct, int item) {
+        driver.findElement(By.name(addCartProduct)).click();
+        WebElement cartItem = driver.findElement(By.cssSelector("span.quantity"));
+        int itemsExpectedNumber = item + 1;
+        String itemsExpectedNumberAsString = Integer.toString(itemsExpectedNumber);
+        wait.until(attributeContains(cartItem, "textContent", itemsExpectedNumberAsString));
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.className("general-0")).click();
 
     }
 
